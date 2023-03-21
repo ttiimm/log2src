756c620ba1fe19ea9f21539e181fa55e7880fbd8
Tim Likarish <likarish@gmail.com>
2023-03-20 22:40:47 -0400

    [logdbg] Capture thread id when reading logs
    Add some command line args that allow reading from a file and specifying a thread
    identifier to use when processing the input. The thread id will be used to identify
    which line belongs to which thread.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,43 @@
-use std::{env, io, fs};
+use clap::Parser;
+use regex::Regex;
+
+use std::{io, fs, path::PathBuf, error::Error};
 
 
-fn main() {
-    let input = env::args().nth(1);
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long, value_name = "FILE")]
+    path: Option<PathBuf>,
+
+    thread_id: String,
+}
+
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Cli::parse();
+
+    let thread_re: Regex = Regex::new(&args.thread_id)?;
+
+    let input = args.path;
     let mut reader: Box<dyn io::Read> = match input {
         None => Box::new(io::stdin()),
         Some(filename)   => Box::new(fs::File::open(filename).unwrap())
     };
 
     let mut buffer = String::new();
-    reader.read_to_string(&mut buffer);
-
-    for line in buffer.split("\n") {
-        println!("{}", line);
+    reader.read_to_string(&mut buffer)?;
+    let results: Vec<(&str, &str)> = buffer.lines()
+        .filter_map(|line| {
+            match thread_re.captures(line) {
+                Some(capture) => Some((capture.get(0).unwrap().as_str(), line)),
+                _ => None
+            }
+        })
+        .collect();
+    for r in results {
+        println!("{} -> {}", r.0, r.1);
     }
+    Ok(())
 }
