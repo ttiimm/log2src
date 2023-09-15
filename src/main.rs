033983654352651c310b362c3c3b55d8d9a78905
Tim Likarish <likarish@gmail.com>
2023-09-14 23:35:38 -0400

    [logdbg] Use tree-sitter to parse logging debug strings
    Try extracting the log statements from source using the tree-sitter parsing library.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use clap::Parser;
+use clap::Parser as ClapParser;
 use regex::Regex;
-
 use std::{io, fs, path::PathBuf, error::Error};
+use tree_sitter::{Parser, Query, QueryCursor};
 
 
-#[derive(Parser)]
+#[derive(ClapParser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     #[arg(short, long, value_name = "FILE")]
@@ -32,10 +32,46 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut buffer = String::new();
     reader.read_to_string(&mut buffer)?;
-    let results = logdbg::filter_log(&buffer, thread_re);
+    let filtered = logdbg::filter_log(&buffer, thread_re);
 
-    for r in results {
+    for r in filtered {
         println!("{} -> {}", r.0, r.1);
     }
+
+    let mut parser = Parser::new();
+    parser.set_language(tree_sitter_rust::language()).expect("Error loading Rust grammar");
+
+    let source = fs::read_to_string("examples/basic.rs")
+        .expect("Expecting a readable file");
+    let tree = parser.parse(&source, None).unwrap();
+    let root_node = tree.root_node();
+
+    let log_strings = r#"(macro_invocation
+                                 macro: (identifier) @macro-name
+                                   (token_tree (string_literal) @log)
+                                 (#eq? @macro-name "debug"))"#;
+    let query = Query::new(tree_sitter_rust::language(), log_strings)
+        .unwrap();
+    let mut query_cursor = QueryCursor::new();
+    let matches = query_cursor.matches(
+        &query, root_node, source.as_bytes()
+    );
+
+    for m in matches {
+        for capture in m
+            .captures
+            .iter()
+            .filter(|c| c.node.kind() == "string_literal") {
+                let range = capture.node.range();
+                let text = &source[range.start_byte..range.end_byte];
+                let line = range.start_point.row + 1;
+                let col = range.start_point.column;
+                println!(
+                    "[Line: {}, Col: {}] Offending source code: `{}`",
+                    line, col, text
+                );
+        }
+    }
+
     Ok(())
 }
