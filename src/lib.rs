d741cfb4a03b8225fd6cebe7d67e185291fb5e75
Tim Likarish <likarish@gmail.com>
2023-09-18 22:39:34 -0400

    [logdbg] Move source filtering into lib
    Moving out the source filtering code into the library.
    
    TODO:
    * Work on data structure to associate the logs with source
    * Try and make the CLI better -- probably remove the stdin
    * Regex based filtering?

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,56 @@
 use regex::Regex;
+use std::fs;
+use tree_sitter::{Parser, Query, QueryCursor};
 
-pub fn filter_log(buffer: &String, thread_re: Regex) -> Vec<(&str, &str)> {
-    let results: Vec<(&str, &str)> = buffer.lines()
+
+pub fn filter_log(buffer: &String, thread_re: Regex) -> Vec<String> {
+    let results: Vec<String> = buffer.lines()
         .filter_map(|line| {
             match thread_re.captures(line) {
-                Some(capture) => Some((capture.get(0).unwrap().as_str(), line)),
+                Some(capture) => {
+                    let result = format!("{} -> {}", capture.get(0).unwrap().as_str(), line);
+                    Some(result)
+                },
                 _ => None
             }
         })
         .collect();
     results
+}
+
+pub fn filter_source(source_path: &str) -> Vec<String> {
+    let mut parser = Parser::new();
+    parser.set_language(tree_sitter_rust::language()).expect("Error loading Rust grammar");
+
+    let source = fs::read_to_string(source_path)
+        .expect("Can read the source file");
+    let tree = parser.parse(&source, None).unwrap();
+    let root_node = tree.root_node();
+
+    let log_strings = r#"(macro_invocation
+                                macro: (identifier) @macro-name
+                                (token_tree (string_literal) @log)
+                                (#eq? @macro-name "debug"))"#;
+    let query = Query::new(tree_sitter_rust::language(), log_strings)
+        .unwrap();
+    let mut query_cursor = QueryCursor::new();
+    let matches = query_cursor.matches(
+        &query, root_node, source.as_bytes()
+    );
+
+    let mut matched = Vec::new();
+    for m in matches {
+        for capture in m
+            .captures
+            .iter()
+            .filter(|c| c.node.kind() == "string_literal") {
+                let range = capture.node.range();
+                let text = &source[range.start_byte..range.end_byte];
+                let line = range.start_point.row + 1;
+                let col = range.start_point.column;
+                let result = format!("[Line: {}, Col: {}] source `{}`", line, col, text);
+                matched.push(result);
+        }
+    };
+    matched
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use clap::Parser as ClapParser;
+use logdbg::filter_source;
 use regex::Regex;
 use std::{io, fs, path::PathBuf, error::Error};
-use tree_sitter::{Parser, Query, QueryCursor};
 
 
 #[derive(ClapParser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    #[arg(short, long, value_name = "FILE")]
-    path: Option<PathBuf>,
+    #[arg(short, long, value_name = "LOG")]
+    log: Option<PathBuf>,
 
     #[arg(short, long, value_name = "THREADID")]
     thread_id: Option<String>,
+
+    #[arg(short, long, value_name = "SOURCE")]
+    source: String
 }
 
 
@@ -24,7 +27,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         Regex::new(&".").expect("Regex failed")
     };
 
-    let input = args.path;
+    let input = args.log;
     let mut reader: Box<dyn io::Read> = match input {
         None => Box::new(io::stdin()),
         Some(filename)   => Box::new(fs::File::open(filename).unwrap())
@@ -33,45 +36,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut buffer = String::new();
     reader.read_to_string(&mut buffer)?;
     let filtered = logdbg::filter_log(&buffer, thread_re);
+    print_result(filtered);
 
-    for r in filtered {
-        println!("{} -> {}", r.0, r.1);
-    }
-
-    let mut parser = Parser::new();
-    parser.set_language(tree_sitter_rust::language()).expect("Error loading Rust grammar");
-
-    let source = fs::read_to_string("examples/basic.rs")
-        .expect("Expecting a readable file");
-    let tree = parser.parse(&source, None).unwrap();
-    let root_node = tree.root_node();
-
-    let log_strings = r#"(macro_invocation
-                                 macro: (identifier) @macro-name
-                                   (token_tree (string_literal) @log)
-                                 (#eq? @macro-name "debug"))"#;
-    let query = Query::new(tree_sitter_rust::language(), log_strings)
-        .unwrap();
-    let mut query_cursor = QueryCursor::new();
-    let matches = query_cursor.matches(
-        &query, root_node, source.as_bytes()
-    );
-
-    for m in matches {
-        for capture in m
-            .captures
-            .iter()
-            .filter(|c| c.node.kind() == "string_literal") {
-                let range = capture.node.range();
-                let text = &source[range.start_byte..range.end_byte];
-                let line = range.start_point.row + 1;
-                let col = range.start_point.column;
-                println!(
-                    "[Line: {}, Col: {}] Offending source code: `{}`",
-                    line, col, text
-                );
-        }
-    }
-
+    let source_path = "examples/basic.rs";
+    let matched = filter_source(source_path);
+    print_result(matched);
     Ok(())
+}
+
+fn print_result(result: Vec<String>) {
+    for r in result {
+        println!("{}", r);
+    }
 }
