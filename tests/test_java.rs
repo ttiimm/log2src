124ec317f4166f8f97ee1e8c5d79b90cdffb526c
Tim Likarish <likarish@gmail.com>
2024-05-09 16:14:19 -0400

    [Test] WIP Java example

--- tests/test_java.rs
+++ tests/test_java.rs
@@ -1,10 +1,22 @@
-use std::path::Path;
+use assert_cmd::prelude::*;
+use std::{path::Path, process::Command};
 
 #[test]
-fn basics() {
+fn basic() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("log2src")?;
+    let basic_source = Path::new("tests").join("java").join("Basic.java");
     let basic_log = Path::new("tests")
         .join("resources")
         .join("java")
         .join("basic.log");
-    assert!(basic_log.exists())
+    cmd.arg("-d")
+        .arg(basic_source.to_str().expect("test case source code exists"))
+        .arg("-l")
+        .arg(basic_log.to_str().expect("test case log exists"));
+    cmd.assert().success().stdout(r#"{"srcRef":{"sourcePath":"tests/java/Basic.java","lineNumber":18,"column":16,"name":"main","text":"\"Hello from main\"","vars":[]},"variables":{},"stack":[]}
+{"srcRef":{"sourcePath":"tests/java/Basic.java","lineNumber":25,"column":16,"name":"foo","text":"\"Hello from foo i={}\"","vars":["i"]},"variables":{"i":"0"},"stack":[]}
+{"srcRef":{"sourcePath":"tests/java/Basic.java","lineNumber":25,"column":16,"name":"foo","text":"\"Hello from foo i={}\"","vars":["i"]},"variables":{"i":"1"},"stack":[]}
+{"srcRef":{"sourcePath":"tests/java/Basic.java","lineNumber":25,"column":16,"name":"foo","text":"\"Hello from foo i={}\"","vars":["i"]},"variables":{"i":"2"},"stack":[]}
+"#);
+    Ok(())
 }
