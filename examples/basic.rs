d534117d1cbb3b4a9a9651d462e716a1491dedc3
Tim Stack <timothyshanestack@gmail.com>
2025-09-03 20:40:48 -0700

    [tests] fix filter since backslashes are escaped in JSON and add named arg test

--- examples/basic.rs
+++ examples/basic.rs
@@ -7,8 +7,11 @@ fn main() {
     for i in 0..3 {
         foo(i);
     }
+    bar(4);
 }
 
 fn foo(i: u32) {
     debug!("Hello from foo i={}", i);
 }
+
+fn bar(j: u32) { debug!("Hello from bar j={j}"); }
