e774792ffe1a3333dcc2c915fce9dc1eb5672c67
Tim Stack <timothyshanestack@gmail.com>
2025-09-08 14:58:43 -0700

    [tests] fix windows variances

--- tests/common_settings.rs
+++ tests/common_settings.rs
@@ -1,6 +1,7 @@
 pub fn enable_filters() -> insta::internals::SettingsBindDropGuard {
     let mut settings = insta::Settings::clone_current();
-    settings.add_filter(r#""examples(?:/|\\\\)"#, "\"{example_dir}/");
-    settings.add_filter(r#""tests(?:/|\\\\)java(?:/|\\\\)"#, "\"{java_dir}/");
+    settings.add_filter(r#""examples(?:/|\\\\?)"#, "\"{example_dir}/");
+    settings.add_filter(r#""tests(?:/|\\\\?)java(?:/|\\\\?)"#, "\"{java_dir}/");
+    settings.add_filter(r#"(?:[ \w\.]+) (\(os error \d+\))"#, " {errmsg} $1");
     settings.bind_to_scope()
 }
