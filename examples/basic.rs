ca1a41805c716a980c223d8d448326abbaae6451
Tim Likarish Ellis <likarish@gmail.com>
2026-05-27 10:41:22 -0400

    [Cargo] format

--- examples/basic.rs
+++ examples/basic.rs
@@ -15,6 +15,10 @@ fn foo(i: u32) {
     debug!("Hello from foo i={}", i);
 }
 
-fn bar(j: u32) { debug!("Hello from bar j={j}"); }
+fn bar(j: u32) {
+    debug!("Hello from bar j={j}");
+}
 
-fn baz(i: u32, j: u32) { debug!("Hello from baz i={1} j={0}", j, i); }
+fn baz(i: u32, j: u32) {
+    debug!("Hello from baz i={1} j={0}", j, i);
+}
